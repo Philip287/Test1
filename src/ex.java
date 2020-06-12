865e8d4fab5d215f7daf947ef2cc05df99f31151
Nikita Bolotin <SDmitry90@Gmail.com>
2020-06-12 19:57:08 +0300

    МНЕ ИНТЕРЕСНО

--- src/ex.java
+++ src/ex.java
@@ -1,6 +1,7 @@
 public class ex {
 
-        public static void main(String []args){
-
-        }
+    public static void main(String[] args) {
+        System.out.println("hELLO WORLD");
+        System.out.println("hELLO WORLD");
+    }
 }
