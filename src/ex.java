ec267aa086314f06efed16ca14176e9aca532040
Nikita Bolotin <SDmitry90@Gmail.com>
2020-06-12 20:47:04 +0300

    МНЕ ИНТЕРЕСНО

--- src/ex.java
+++ src/ex.java
@@ -1,7 +1,56 @@
 public class ex {
 
     public static void main(String[] args) {
-        System.out.println("hELLO WORLD");
-        System.out.println("hELLO WORLD");
+       /* int number1 = 17;
+        int number2 = 21;
+
+        System.out.println(number1 + number2);
+        System.out.println(number1 - number2);
+        System.out.println(number1 * number2);
+        System.out.println(number1 / number2);
+
+        int initial = 80;
+        float floatVar = 27.34534534f;
+        double doubleVar = 27.34534534;
+        System.out.println(initial - floatVar);
+*/
+        int var = 10;
+       /* System.out.println(var);
+        System.out.println(var++);
+        System.out.println(var);
+        System.out.println();
+        var = 10;
+        System.out.println(var);
+        System.out.println(++var);
+        System.out.println(var);
+        System.out.println();
+
+
+        byte number1 = 17;
+        byte number2 = 21;
+        System.out.println(Integer.toBinaryString(-17));
+        System.out.println(Integer.toBinaryString(~number1));
+        System.out.println(Integer.toBinaryString(number1 & number2));
+        System.out.println(Integer.toBinaryString(number1 | number2));
+        System.out.println(Integer.toBinaryString(number1));
+        System.out.println(Integer.toBinaryString(number1 >> 1));
+        System.out.println(Integer.toBinaryString(number1 >> 2));
+        System.out.println(Integer.toBinaryString(number1 >> 3));
+        System.out.println(Integer.toBinaryString(number1 >> 4));
+        System.out.println(Integer.toBinaryString(number1 >> 5));
+        System.out.println(number1 == number2);
+        System.out.println(number1 != number2);
+  byte number1 = 17;
+        byte number2 = 21;
+        int max = number1 > number2
+                ? number1
+                : number2;
+
+*/
+        byte number1 = 17;
+        byte number2 = 21;
+
+        int number3 =
+
     }
 }
